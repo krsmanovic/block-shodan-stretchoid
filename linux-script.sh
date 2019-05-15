d7fb461f9435e00b5827dbf9c0b841f771c81476
Čedomir Krsmanović <30358338+krsmanovic@users.noreply.github.com>
2019-05-15 16:00:11 +0200

    Added logging facility

--- linux-script.sh
+++ linux-script.sh
@@ -9,6 +9,11 @@
 # those credentials for logging in
 router_ssh="ssh script_user@192.168.0.1 -p 22"
 
+# Logging facility parameters
+log_dir="/home/$(id -un)/scripts/fw_log"
+log_file="$log_dir/$(date +%Y-%m)_bad_hosts.log"
+log_file_alert="$log_dir/$(date +%Y-%m)_other_hosts.log"
+
 # Stage 1 dynamic address list on the remote MikroTik
 router_stage1="fw_stage1"
 
@@ -19,6 +24,10 @@ router_blacklist="fw_blacklist"
 file="stage1"
 
 > "$file"
+
+if [[ ! -d $log_dir ]]; then
+  mkdir -p $log_dir
+fi
 
 $router_ssh "/ip firewall address-list print where list=$router_stage1" | awk 'NR > 2 {print $4}' >> "$file"
 
@@ -34,6 +43,19 @@ do
     then
         $router_ssh -n "do { /ip firewall address-list add list=$router_blacklist comment=$domain address=$ip } on-error={}"
         echo "Host $ip with PTR $domain was added to the blacklist on the remote device."
+        if [[ -f $log_file  ]]; then
+          echo -e $(date +%Y-%m-%d) $ip"\t\t"$ptr >> $log_file
+        else
+          touch $log_file
+          echo -e $(date +%Y-%m-%d) $ip"\t\t"$ptr >> $log_file
+        fi
+    else
+        if [[ -f $log_file_alert  ]]; then
+          echo -e $(date +%Y-%m-%d) $ip "\t\t"$ptr >> $log_file_alert
+        else
+          touch $log_file_alert
+          echo -e $(date +%Y-%m-%d) $ip "\t\t"$ptr >> $log_file_alert
+        fi
     fi
   fi
 done < "$file"
